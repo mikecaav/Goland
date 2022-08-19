d33849075291b091c0b0ff521d11336ebd63263d
Miguel Caballero <caballero.mc98@gmail.com>
2022-08-19 16:37:41 -0500

    added tcp simple socket connection

---
+++ TCP/basiTcp.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "8080"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+	listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+
+	defer listener.Close()
+	log.Println("Listening on " + CONN_HOST)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Error accepting", err.Error())
+		}
+		log.Print(conn)
+	}
+}
