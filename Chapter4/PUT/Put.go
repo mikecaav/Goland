47ba3dc8cd667acc051562dd8e1f56b007524bc8
Miguel Caballero <caballero.mc98@gmail.com>
2022-08-31 06:27:16 -0500

    added put method

---
+++ Chapter4/PUT/Put.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "8080"
+)
+
+type Employee struct {
+	Id        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+type Employees []Employee
+
+type Route struct {
+	Path        string
+	Name        string
+	HandlerFunc http.HandlerFunc
+	Method      string
+}
+
+type Routes []Route
+
+var employees Employees
+var routes Routes
+
+func init() {
+	employees = Employees{
+		Employee{1, "Miguel", "Caballero"},
+		Employee{2, "Benjamin", "Caballero"},
+		Employee{3, "Zaira", "Avendaño"},
+	}
+	routes = Routes{
+		Route{Name: "getEmployees", HandlerFunc: getEmployees, Path: "/employees", Method: "GET"},
+		Route{Name: "addEmployee", HandlerFunc: addEmployee, Path: "/employee", Method: "POST"},
+		Route{Name: "addEmployee", HandlerFunc: updateEmployee, Path: "/employee", Method: "PUT"},
+	}
+}
+
+func getEmployees(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(employees)
+}
+
+func addEmployee(w http.ResponseWriter, r *http.Request) {
+	employee := Employee{}
+	err := json.NewDecoder(r.Body).Decode(&employee)
+	if err != nil {
+		log.Print("error occurred while decoding employee data :: ", err)
+		return
+	}
+	employees = append(employees, employee)
+	json.NewEncoder(w).Encode(employees)
+}
+
+func updateEmployee(w http.ResponseWriter, r *http.Request) {
+	employee := Employee{}
+	err := json.NewDecoder(r.Body).Decode(&employee)
+	if err != nil {
+		log.Print("error occurred while decoding employee data :: ", err)
+		return
+	}
+
+	var foundId = false
+	for idx, employee_ := range employees {
+		if employee.Id == employee_.Id {
+			foundId = true
+			employees[idx] = employee
+		}
+	}
+
+	if !foundId {
+		log.Println("Employee not found")
+	}
+
+	json.NewEncoder(w).Encode(employees)
+}
+
+func AddRoute(muxRouter *mux.Router) *mux.Router {
+	for _, route := range routes {
+		muxRouter.Name(route.Name).Path(route.Path).Methods(route.Method).Handler(route.HandlerFunc)
+	}
+
+	return muxRouter
+}
+
+func main() {
+	var muxRouter = mux.NewRouter().StrictSlash(true)
+	muxRouter = AddRoute(muxRouter)
+	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, muxRouter)
+	if err != nil {
+		log.Fatal("error starting http server : ", err)
+		return
+	}
+}
